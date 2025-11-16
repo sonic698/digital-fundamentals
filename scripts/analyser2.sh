bc73f12b39c0dbd166d7a4bc9f83f352a14ff32f
sonic698 <ishkovasonya@yandex.ru>
2025-11-16 15:16:39 +0300

    Update analyser2.sh

--- scripts/analyser2.sh
+++ scripts/analyser2.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+for i in $(ls $1)
+do
+for file in $(cat $1$i)
+do
+email=$(echo $file | cut -d ":" -f3)
+if [[ $email =~ @spbstu.ru ]]
+then 
+name=$(echo $file | cut -d ":" -f1)
+age=$(echo $file | cut -d ":" -f2)
+echo -e "Имя: $name, возраст: $age , почта: $email"
+fi
+done
+done
