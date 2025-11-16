3a456cc7b57750c083abf8384784fed3a4988bf0
sonic698 <ishkovasonya@yandex.ru>
2025-11-16 15:17:05 +0300

    Update analyser3.sh

--- scripts/analyser3.sh
+++ scripts/analyser3.sh
@@ -1,1 +1,17 @@
-
+#!/bin/bash
+sov=0
+nesov=0
+for i in $(ls $1)
+do
+for file in $(cat $1$i)
+do
+age=$(echo $file | cut -d ":" -f2)
+if [[ $age -gt 18 ]]
+then
+sov=$(($sov + 1))
+else
+nesov=$(($nesov + 1))
+fi
+done
+done
+echo -e "совершеннолетних - $sov, несовершеннолетних - $nesov"
