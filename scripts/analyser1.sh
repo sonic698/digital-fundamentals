524e5b2826fa25837000b6b7d782319427473f4d
sonic698 <ishkovasonya@yandex.ru>
2025-11-16 15:16:13 +0300

    Update analyser1.sh

--- scripts/analyser1.sh
+++ scripts/analyser1.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+for i in $(ls $1)
+do
+for file in $(cat $1$i)
+do
+name=$(echo $file | cut -d ":" -f1)
+age=$(echo $file | cut -d ":" -f2)
+email=$(echo $file | cut -d ":" -f3)
+echo -e "Имя: $name | возраст: $age | почта: $email"
+done
+done
