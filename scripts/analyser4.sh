0585374432b663c8eeed87d7ebea5223ac025e47
sonic698 <ishkovasonya@yandex.ru>
2025-11-16 15:17:28 +0300

    Update analyser4.sh

--- scripts/analyser4.sh
+++ scripts/analyser4.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+min=100; max=-1
+for i in $(ls $1)
+do
+for file in $(cat $1$i)
+do
+age=$(echo $file | cut -d ":" -f2)
+#echo $age
+if [ $age -lt $min ]
+then
+min=$age
+elif [ $age -gt $max ]
+then 
+max=$age
+fi
+done
+done
+echo $min
+echo $max
